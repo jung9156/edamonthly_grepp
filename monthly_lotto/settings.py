fb3ee29cb3d47dc9ae16459973081207a6ac0208
Ubuntu <ubuntu@ip-172-31-42-137.ap-northeast-2.compute.internal>
2021-05-20 05:40:45 +0000

    wow

--- monthly_lotto/settings.py
+++ monthly_lotto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(mbhzmj=ofbo0kyryz^8gn$z9%tyssmqjzqt@ol@=!)o5kl^bj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
